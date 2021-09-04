e199b1234253a1690c87212b57ee638a8d9c04cd
Karol Gucwa <karolus2212@gmail.com>
2021-09-05 00:03:42 +0200

    transfered

--- src/views/Root.js
+++ src/views/Root.js
@@ -3,7 +3,7 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from 'assets/styles/GlobalStyle';
 import { theme } from 'assets/styles/theme';
 import Navigation from 'components/templates/Navigation/Navigation';
-import HeaderPhotos from 'components/atoms/HeaderPhotos/HeaderPhotos';
+import HeaderPhotos from 'components/molecules/HeaderPhotos/HeaderPhotos';
 import Quote from 'components/atoms/Quote/Quote';
 
 const Root = () => {
