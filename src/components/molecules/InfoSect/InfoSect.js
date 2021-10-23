02f122510dd0e15f7cac0d692421191300bcc7d0
Karol Gucwa <karolus2212@gmail.com>
2021-10-23 16:12:32 +0200

    added: strona w trakcie prac

--- src/components/molecules/InfoSect/InfoSect.js
+++ src/components/molecules/InfoSect/InfoSect.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import { Section } from './InfoSect.styles';
+import { Section, SiteIsBuildingInfo } from './InfoSect.styles';
+import { ReactComponent as WarningIcon } from 'icons/iconmonstr-warning-6.svg';
 
 const InfoSect = () => {
   return (
-    <Section as="section">
-      <h1>Walduś - firma ogólnobudowlana</h1>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since
-        the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of
-        Letraset sheets
-      </p>
-    </Section>
+    <>
+      <SiteIsBuildingInfo>
+        <WarningIcon />
+        Strona w trakcie prac
+        <WarningIcon />
+      </SiteIsBuildingInfo>
+      <Section as="section">
+        <h1>Walduś - firma ogólnobudowlana</h1>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five
+          centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release
+          of Letraset sheets
+        </p>
+      </Section>
+    </>
   );
 };
 
